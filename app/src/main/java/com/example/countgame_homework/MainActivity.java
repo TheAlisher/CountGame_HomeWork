fdcb95e9e0310f2f7bba5fad240afff7899d9087
macbookpro <abenov2005.uzer@gmail.com>
2020-04-27 11:23:10 +0600

    Initial commit

---
+++ app/src/main/java/com/example/countgame_homework/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.countgame_homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView WinOrLose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int a = 1;
+        int b = 100;
+        final int random_number = a + (int) (Math.random() * b );
+
+        Button answer = findViewById(R.id.answer_Button);
+        answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (seekBar.getProgress() == random_number) {
+                    WinOrLose.setText("Угадал");
+                } else {
+                    WinOrLose.setText("Не угадал");
+                }
+            }
+        });
+
+        WinOrLose = findViewById(R.id.win_or_lose_TextView);
+
+        seekBar = findViewById(R.id.SeekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
